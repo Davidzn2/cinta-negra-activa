f506fbd4452eb74b45f48e1e7e1b08957c07cf62
davidz <davidzn2@gmail.com>
2020-07-21 17:10:22 -0500

    Api, params y query

--- 02-API/index.js
+++ 02-API/index.js
@@ -4,9 +4,30 @@ const app = express();
 app.get('/', (request, response)=>{
     response.send('Hola mundo')
 })
-app.get('/chapan', (request, response)=>{
+app.get('/rick', (request, response)=>{
     response.send({
         'character': 'Rick sanchez'
     })
 })
-app.listen(8000, ()=> console.log(`Esta vivo en el puerto 8000`))
+app.get('/saludar/:name/:lastname', (request, response)=>{
+    console.log(request.query, request.params)
+    const { name, lastname } = request.params;
+    if(name === 'david'){
+        response.status(200).json({'respuesta':`${name} ${lastname}, bienvenido`})
+
+    }else{
+        response.status(400).json(`denegado`)
+    }
+})
+
+app.get('/mayores/', (request, response)=>{
+    console.log(request.query)
+    const { edad } = request.query
+    if (edad >= 18){
+        response.send('Bienvenido')
+    } else{
+        response.send('Vete Menor!!')
+    }
+})
+
+app.listen(7000, ()=> console.log(`Esta vivo en el puerto 7000`))
