ae9a396941d8132fce09ee66f21306dafec1e337
davidz <davidzn2@gmail.com>
2020-07-23 17:30:12 -0500

    CRUD Completo

--- 03-DB/index.js
+++ 03-DB/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const { config } = require("./config");
 
 const app = express();
+
+// CONEXIÓNN A LA BASE DE DATOS
 mongoose
   .connect(config.db.url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Conectado!!!"))
@@ -18,17 +20,46 @@ const equiposSchema = new mongoose.Schema({
   clasifico: Boolean,
 });
 // MODELO, -> OBJETO QUE ME DEJA INTERACTUAR CON LA COLECCION DE MONGO DB
-const Equipos = mongoose.model('Equipos', equiposSchema);
+const Equipos = mongoose.model("Equipos", equiposSchema);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 
 app.get("/", (request, response) => {
   response.send("It works");
 });
 app.get("/equipos", (request, response) => {
-    Equipos.find()
-        .then((resDB) => response.status(200).json(resDB))
-        .catch((err) => response.status(400).json(err));
+  Equipos.find()
+    .then((respDB) => response.status(200).json(respDB))
+    .catch((err) => response.status(400).json(err));
 });
 
+app.get("/equipos/:id", (request, response) => {
+  Equipos.findById(request.params.id)
+    .then((respDB) => response.status(200).json(respDB))
+    .catch((err) => response.status(400).json(err));
+});
+app.post("/equipos", (request, response) => {
+  // 1. Recibir la info que me manda el cliente
+  const { body } = request;
+
+  // 2. Pedirle a la DB que cree un documento en la colleccion
+  // Con la info del body
+  const newEquipo = new Equipos(body);
+  newEquipo.save()
+    .then((respDB) => response.status(201).json(respDB))
+    .catch((err) => response.status(400).json(err));
+});
+app.patch("/equipos/:id", (request, response)=>{
+  Equipos.findByIdAndUpdate(request.params.id, request.body)
+  .then((respDB) => response.status(200).json(respDB))
+  .catch((err) => response.status(400).json(err));
+})
+app.delete("/equipos/:id", (request, response) => {
+  Equipos.findByIdAndDelete(request.params.id)
+    .then((respDB) => response.status(204).json(respDB))
+    .catch((err) => response.status(400).json(err));
+});
 app.listen(config.port, () =>
   console.log(`Api Listening on port: ${config.port}`)
 );
